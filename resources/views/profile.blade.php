5c846dcfc7611fa5bb538fcaa3967fcc20fe4803
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2026-02-10 22:07:09 +0100

    Bonus create navbar anco link page views

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,19 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
     <title>Profile</title>
 </head>
 <body>
     <header>
-        <nav>
-            <a href="">Home</a>
-            <a href="">Profile</a>
-            <a href="">Dashboard</a>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="{{route('home')}}" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('profile')}}" class="nav-link active">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}" class="nav-link">Dashboard</a>
+                </li>
+            </ul>
         </nav>
     </header>
     <main>
   
-        <h1>Profile</h1>
+        <h1 class="m-3">Profile</h1>
 
     </main>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/Profile', function () {
+    return view('profile');
+})->name('profile');
+
+Route::get('/Dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
