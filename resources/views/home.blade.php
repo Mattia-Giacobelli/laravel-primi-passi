225434db2bdf49537d036303e8d6b8b6a25a8ddd
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2026-02-10 21:50:19 +0100

    Create home page view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Profile</a>
+            <a href="">Dashboard</a>
+        </nav>
+    </header>
+    <h1>Hello World</h1>
+
+</body>
+</html>
